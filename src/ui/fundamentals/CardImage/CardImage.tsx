862ebe3b9b6de45e87265b82a566a93aa4d34a77
ui-d <dawiddeveloper@gmail.com>
2024-02-25 20:08:59 +0100

    feat: card image component

--- src/ui/fundamentals/CardImage/CardImage.tsx
+++ src/ui/fundamentals/CardImage/CardImage.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image"
 import { CardImageProps } from "./CardImage.types"
 
-export const CardImage = ({}: CardImageProps) => {
-  return <>CardImage</>
+export const CardImage = (props: CardImageProps) => {
+  const { className, src, alt, placeholder, width, height, ...domProps } = props
+  return <Image className={`relative ${className}`} src={src} alt={alt} {...domProps} />
 }
--- src/ui/fundamentals/CardImage/CardImage.types.ts
+++ src/ui/fundamentals/CardImage/CardImage.types.ts
@@ -1,1 +1,12 @@
-export type CardImageProps = {}
+import type { HTMLAttributes } from "react"
+
+export type CardImageBaseProps = {
+  src: string
+  alt: string
+  className?: string
+  placeholder?: string
+  width?: number
+  height?: number
+}
+
+export type CardImageProps = CardImageBaseProps & HTMLAttributes<HTMLImageElement>
