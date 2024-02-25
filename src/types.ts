69d76c581e4c9dc5e595272da8a1f2af6b4d5f05
ui-d <dawiddeveloper@gmail.com>
2024-02-25 16:53:45 +0100

    chore: types added

---
+++ src/types.ts
@@ -0,0 +1,23 @@
+export type sizes = "sm" | "md" | "lg"
+
+export type Cars = {
+  cars: Car[]
+}
+
+export type Car = {
+  id: string
+  link: Link
+  title: string
+  brand: string
+  vehicleType: VehicleType
+}
+
+export type Link = {
+  href: string
+  title: string
+}
+
+export enum VehicleType {
+  Pb = "PB",
+  Su = "SU",
+}
